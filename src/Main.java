38593cfa128f4bdbc30874870e0e56068a39edf3
yulescrob <122058516+yulescrob@users.noreply.github.com>
2023-02-05 21:25:42 -0500

    Completed task 1 of lab 4

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
+    double price = 350.99;
+    double taxRate = 0.05;
+    double salesTax;
+    salesTax = price * taxRate;
+        System.out.println("The price of your purchase is " + price + " and the sales tax is " + salesTax);
 
 
 
